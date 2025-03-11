d1e68e3f4256d501b38c32bc5c62248a10673af0
Mainatonny <antony.macharia21@dkut.students.dkut.ac.ke>
2025-03-11 08:20:24 +0300

    Updated PayPal config for Live Mode

--- config/paypalClient.js
+++ config/paypalClient.js
@@ -1,21 +1,21 @@
-// config/paypalClient.js
 const paypal = require('@paypal/checkout-server-sdk');
 
 const getEnvironment = () => {
-  if (process.env.NODE_ENV === 'development') {
-    // Use Sandbox for development
+  if (process.env.PAYPAL_MODE === 'sandbox') {
+    // Use Sandbox for testing
     return new paypal.core.SandboxEnvironment(
       process.env.PAYPAL_CLIENT_ID,
-      process.env.PAYPAL_CLIENT_SECRET
+      process.env.PAYPAL_SECRET
     );
   } else {
-    // Use Live in production
+    // Use Live for real transactions
     return new paypal.core.LiveEnvironment(
       process.env.PAYPAL_CLIENT_ID,
-      process.env.PAYPAL_CLIENT_SECRET
+      process.env.PAYPAL_SECRET
     );
   }
 };
 
 const client = new paypal.core.PayPalHttpClient(getEnvironment());
 module.exports = client;
+
